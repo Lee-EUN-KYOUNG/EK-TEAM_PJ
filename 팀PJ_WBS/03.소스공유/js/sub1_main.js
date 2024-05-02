609af90639535685205e100d463164f18f348af8
EUNKYOUNG <amurca@naver.com>
2024-05-02 18:11:05 +0900

    111111

--- 팀PJ_WBS/03.소스공유/js/sub1_main.js
+++ 팀PJ_WBS/03.소스공유/js/sub1_main.js
@@ -14,7 +14,7 @@ import * as sub1_data from "./sub1_data.js";
     // 변경 대상 : .trmovie-box
     const clipBox = mFn.qs(".trmovie-box");
   
-    // 5-2. 생성 코드 변수
+    // 생성 코드 변수
     let hcode = `<ul class="trintro">`;
   
     sub1_data.clipData.forEach((v) => {
@@ -32,9 +32,49 @@ import * as sub1_data from "./sub1_data.js";
 
   hcode += `</ul>`;
 
-  // 5-3. 화면 출력하기
+  // 화면 출력하기
   clipBox.innerHTML = hcode;
+
 })(); /////////////// 인트로 파트 영역 끝
+
+/// 디자인 파트
+(() => {
+  const deBox = mFn.qs(".design-box")
+  let gcode = `<ul class="deintro">`;
+  sub1_data.designData.forEach((v) => {
+    gcode += `
+    <h3>${v.title01}</h3>
+    <li class="demovie">
+    <iframe src="https://www.youtube.com/embed/${v.mvid01}" autoplay="트레일러" controls=""></iframe>  
+    <iframe src="https://www.youtube.com/embed/${v.mvid02}" autoplay="트레일러" controls=""></iframe>  
+    <h4>
+    ${v.subintro}</h4>
+    </h4>
+    </li>
+    `;
+  });
+  gcode += `</ul>`;
+  deBox.innerHTML = gcode;
+})(); /////////////// 디자인 파트 영역 끝
+
+
+
+
+////// 캐릭터 이미지 박스
+// 대상 .cha-box
+const chgrid = mFn.qs(".cha-box");
+
+// 이미지 넣기
+for (let i = 1; i <= 8; i++) {
+  chgrid.innerHTML += `
+      <div>
+        <img src="./IMG/img1/cha0${i}.jpg" alt="캐릭터">
+        <h3>${sub1_data.chaData[i - 1].chaname}</h3>
+      </div>
+      `;
+
+} //////// for /////////
+
 
 
 
