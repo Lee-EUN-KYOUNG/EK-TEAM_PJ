ba3d70259ec239ed055948ca74e116f5b71751a6
EUNKYOUNG <amurca@naver.com>
2024-05-02 15:17:00 +0900

    1111

--- 팀PJ_WBS/03.소스공유/js/sub1_data.js
+++ 팀PJ_WBS/03.소스공유/js/sub1_data.js
@@ -1,1 +1,34 @@
 // 서브 1 파트 데이터 JS
+
+
+/************************************* 
+    [ 데이터항목 : ]
+    1.순번(기본키) : idx
+    2.제목 : title
+    3.개요 : subintro
+    4.동영상코드 : mvid
+*************************************/
+const clipData = [
+    {
+      idx: "1",
+      title: "Trailers",
+      subintro: `Ever wonder what toys do when people aren't around? <br>
+      Toy Story answers that question with a fantastic fun-filled journey, <br> viewed mostly through the eyes of two rival toys - Woody, the lanky, likable cowboy, and Buzz Lightyear, the fearless space ranger. <br> Led by Woody, Andy's toys live happily in his room until Andy's birthday brings Buzz Lightyear onto the scene. <br> Afraid of losing his place in Andy's heart, Woody plots against Buzz. <br>
+      But when circumstances separate Buzz and Woody from their owner,
+      the comically-mismatched duo eventually learn to put aside their differences, <br> and they find themselves on a hilarious adventure-filled mission where the only way they can survive is to form an uneasy alliance.`,
+      mvid01: "T_BXsjiFa0M",
+      mvid02: "QqIRLEg521Y",
+    },
+  /*   {
+      idx: "2",
+      title: "Design",
+      subintro: `The filmmakers assembled the Toy Story cast from a wealth of childhood memories.
+      But for the story to work, the two main characters needed to have their own chemistry.
+      Early versions of Woody included a ventriloquist's dummy and an ill-tempered cowboy before he became the affable hero seen on screen. Buzz Lightyear offered a different challenge: inventing a brand-new toy that instantly felt familiar.`,
+      mvid: "4n6u2CqlGPY",
+    }, */
+  ];
+
+
+  // 데이터 공개하기
+export { clipData };
--- 팀PJ_WBS/03.소스공유/js/sub1_main.js
+++ 팀PJ_WBS/03.소스공유/js/sub1_main.js
@@ -1,1 +1,42 @@
 // 서브 1 메인 JS
+
+// 나의 함수 불러오기
+import mFn from "./my_function.js";
+
+// 데이터 셋팅 불러오기
+import * as sub1_data from "./sub1_data.js";
+
+//////////////////////////////////// 데이터 코딩 구역 ////////////////////////////////////////
+
+  // 인트로 파트 영역 시작 //
+  // 데이터 : sub1_data.clipData
+  (() => {
+    // 변경 대상 : .trmovie-box
+    const clipBox = mFn.qs(".trmovie-box");
+  
+    // 5-2. 생성 코드 변수
+    let hcode = `<ul class="trintro">`;
+  
+    sub1_data.clipData.forEach((v) => {
+    hcode += `
+    <li class="trmovie">
+    <h3>${v.title}</h3>
+    <iframe src="https://www.youtube.com/embed/${v.mvid01}" autoplay="트레일러" controls=""></iframe>  
+    <iframe src="https://www.youtube.com/embed/${v.mvid02}" autoplay="트레일러" controls=""></iframe>  
+    <h4>
+    ${v.subintro}</h4>
+    </h4>
+    </li>
+    `;
+  }); ///////// forEach
+
+  hcode += `</ul>`;
+
+  // 5-3. 화면 출력하기
+  clipBox.innerHTML = hcode;
+})(); /////////////// 인트로 파트 영역 끝
+
+
+
+
+
